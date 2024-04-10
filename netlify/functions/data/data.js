8b284b783297018c227cc137b16bb616d16aa28e
“andrewhaddad“ <andrewghaddad@outlook.com>
2024-04-10 17:56:42 -0400

    API integration: adding file as netlify function

--- netlify/functions/data/data.js
+++ netlify/functions/data/data.js
@@ -1,8 +1,26 @@
+// // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// const handler = async (event) => {
+//   try {
+//     const subject = event.queryStringParameters.name || 'World'
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: `Hello ${subject}` }),
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     }
+//   } catch (error) {
+//     return { statusCode: 500, body: error.toString() }
+//   }
+// }
+
+// module.exports = { handler }
+
 const express = require('express');
 const serverless = require('serverless-http')
 const cors = require('cors');
 const axios = require('axios');
-const config = require('./config');
+const config = require('../../../config');
 
 const corsOptions = {
     origin: config.ORIGIN_DOMAIN,
